e74e75ab6d480954786cf1b4bc25447424080ff0
Alain Nambi <alainnambi@gmail.com>
2023-02-22 20:21:35 +0300

    update - only one hyphen for css class

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,23 +11,23 @@ const Header = () => {
         <div className="header">
             <div className="header__container">
                 <div className="header__list">
-                    <div className="header__list--item active">
+                    <div className="header__list-item active">
                         <FontAwesomeIcon icon={faBed} />
                         <span>Stays</span>
                     </div>
-                    <div className="header__list--item">
+                    <div className="header__list-item">
                         <FontAwesomeIcon icon={faPlane} />
                         <span>Flights</span>
                     </div>
-                    <div className="header__list--item">
+                    <div className="header__list-item">
                         <FontAwesomeIcon icon={faCar} />
                         <span>Car rentals</span>
                     </div>
-                    <div className="header__list--item">
+                    <div className="header__list-item">
                         <FontAwesomeIcon icon={faGamepad} />
                         <span>Attractions</span>
                     </div>
-                    <div className="header__list--item">
+                    <div className="header__list-item">
                         <FontAwesomeIcon icon={faTaxi} />
                         <span>Airport taxis</span>
                     </div>
