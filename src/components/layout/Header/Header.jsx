e72c49b3060c10bae5a0a72146efaf0810cbb9c1
Alain Nambi <alainnambi@gmail.com>
2023-02-23 20:28:34 +0300

    update - middle part of header

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -1,5 +1,5 @@
 import { 
-    faBed, faCar, faGamepad, faPlane, faTaxi
+    faBed, faCalendarDays, faCar, faGamepad, faPerson, faPlane, faTaxi
 } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { useState } from "react";
@@ -39,8 +39,40 @@ const Header = () => {
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad aliquid eaque veritatis atque quae alias?
                 </p>
                 <button className="header__button">
-                    SignIn/Register
+                    SignIn / Register
                 </button>
+                <div className="header__search">
+                    <div className="header__search-item">
+                        <FontAwesomeIcon 
+                            icon={faBed} 
+                            className="header__search-item-icon"
+                        />
+                        <input 
+                            type="text" 
+                            className="header__search-input"
+                            placeholder="Where are you going ?"
+                        />
+                    </div>
+                    <div className="header__search-item">
+                        <FontAwesomeIcon 
+                            icon={faCalendarDays} 
+                            className="header__search-item-icon"
+                        />
+                        <span className="header__search-text">Date to date</span>
+                    </div>
+                    <div className="header__search-item">
+                        <FontAwesomeIcon 
+                            icon={faPerson} 
+                            className="header__search-item-icon"
+                        />
+                        <span className="header__search-text">2 Adults 2 children</span>
+                    </div>
+                    <div className="header__search-item">
+                        <button className="header__search-button">
+                            <span className="header__search-text">Search</span>
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     )
